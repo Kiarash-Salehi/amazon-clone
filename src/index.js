2d2bd664a3c663fbc955ab104ff43ad5f39792a5
KSDragon82 <www.kiarashs2929@gmail.com>
2020-10-02 10:54:50 +0330

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reducer, { initialState } from './reducer';
-import { BrowserHistory } from 'react-router-dom';
+import { createBrowserHistory as BrowserHistory } from 'history';
+import { StateProvider } from './StateProvider';
 
 ReactDOM.render(
 	<BrowserHistory base={document.getElementsByTagName('base')[0].getAttribute('href')}>
