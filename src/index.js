180225482dba41858213b4acbd9c125ec6a48bfa
KSDragon82 <www.kiarashs2929@gmail.com>
2020-10-02 10:35:50 +0330

    changed .htaccess file

--- src/index.js
+++ src/index.js
@@ -2,20 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { StateProvider } from './StateProvider';
 import reducer, { initialState } from './reducer';
+import { BrowserHistory } from 'react-router-dom';
 
 ReactDOM.render(
-	<React.StrictMode>
+	<BrowserHistory base={document.getElementsByTagName('base')[0].getAttribute('href')}>
 		<StateProvider initialState={initialState} reducer={ reducer }>
 			<App />
 		</StateProvider>
-	</React.StrictMode>,
+	</BrowserHistory>,
 	document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
